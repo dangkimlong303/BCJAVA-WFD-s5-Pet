cd5ee70c07c1e6555aed4250f1596f4f8623ebe5
dangkimlong303 <dangkimlong303@gmail.com>
2019-01-31 11:15:34 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PetComponent } from './pet/pet.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
